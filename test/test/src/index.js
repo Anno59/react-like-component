8429605c96279e090d0b3e593608e14aabaa29cb
Anno59 <372025284@qq.com>
2018-12-08 11:33:22 +0800

    更新至#14

--- test/test/src/index.js
+++ test/test/src/index.js
@@ -589,7 +589,30 @@ import './index.css';
 // getData(url) 已经可以直接使用
 // 本站的环境都可以使用 async/await
 
-const loadAndRefresh = 'test'; /* TODO */
+//The Key
+// const loadAndRefresh = (url) => (WrappedComponent) => {
+//     return class extends Component {
+//         componentWillMount () {
+//             this._loadData()
+//         }
+//
+//         async _loadData () {
+//             this.setState({ content: '数据加载中...' })
+//             const content = await getData(url)
+//             this.setState({ content })
+//         }
+//         render () {
+//             const props = {
+//                 content: this.state.content,
+//                 refresh: this._loadData.bind(this),
+//                 ...this.props
+//             }
+//             return (
+//                 <WrappedComponent {...props} />
+//             )
+//         }
+//     }
+// }
 
 
 
